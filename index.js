a514dda00c8fb391472e701b78c554d8abff74f9
sandhyasimhadri <sandhyasimhadri999@gmail.com>
2023-07-25 14:12:11 +0530

    inserting newly added data

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const connection = mysql.createConnection({
   database: 'civic_datalab',
 });
 
+
 app.get('/api/create', async (req, res) => {
   try {
     const response = await axios.get('https://data.bmkg.go.id/DataMKG/TEWS/gempadirasakan.json');
@@ -22,7 +23,7 @@ app.get('/api/create', async (req, res) => {
     const earthquakeData = gempa.map((gempa) => [
       gempa.Tanggal,
       gempa.Jam,
-      gempa.DateTime,
+      new Date(gempa.DateTime).toISOString().slice(0, 19).replace('T', ' '),
       gempa.Coordinates,
       gempa.Lintang,
       gempa.Bujur,
@@ -33,63 +34,85 @@ app.get('/api/create', async (req, res) => {
     ]);
 
     const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS earthquakes (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      Date VARCHAR(15),
-      Time VARCHAR(15),
-      DateTime DATETIME,
-      Coordinates VARCHAR(255),
-      Latitude VARCHAR(255),
-      Longitude VARCHAR(255),
-      Magnitude FLOAT,
-      Depth VARCHAR(255),
-      Region TEXT,
-      Felt TEXT
-    )
-  `;
+      CREATE TABLE IF NOT EXISTS earthquakes (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        Date VARCHAR(15),
+        Time VARCHAR(15),
+        DateTime DATETIME,
+        Coordinates VARCHAR(255),
+        Latitude VARCHAR(255),
+        Longitude VARCHAR(255),
+        Magnitude FLOAT,
+        Depth VARCHAR(255),
+        Region TEXT,
+        Felt TEXT
+      )
+    `;
 
-     connection.query(createTableQuery, (err, result) => {
+    connection.query(createTableQuery, (err, result) => {
       if (err) {
         console.log(`Error creating table: ${err}`);
         res.status(500).json({ error: 'Failed to create table' });
       } else {
-        // Insert the earthquake data into the 'earthquakes' table
-       const insert = `
-          INSERT INTO earthquakes (Date, Time, DateTime, Coordinates, Latitude, Longitude, Magnitude, Depth, Region, Felt)
-          VALUES ?
-        `;
-        // Query to insert the data
-        connection.query(insert, [earthquakeData], (err1, result) => {
-          if (err) {
-            console.log(`Error inserting data: ${err1}`);
-            res.status(500).json({ error: 'Failed to insert earthquake data' });
+        const totalDataQuery = `SELECT DateTime FROM earthquakes`;
+        connection.query(totalDataQuery, (err2, existingData) => {
+          if (err2) {
+            console.log(`Error fetching existing earthquake data: ${err2}`);
+            res.status(500).json({ error: 'Failed to fetch existing earthquake data' });
           } else {
-            res.json({message: `Data inserted successfully`});
-            // Do not send the response here, remove the res.json() call
+            let finaldata = earthquakeData;
+
+            existingData.forEach((element) => {
+              finaldata = finaldata.filter((earthquake) => {
+                return new Date(earthquake[2]).toString() !== new Date(element.DateTime).toString();
+              });
+            });
+            //inserting data to the table earthquakes
+            const insert = `
+              INSERT INTO earthquakes (Date, Time, DateTime, Coordinates, Latitude, Longitude, Magnitude, Depth, Region, Felt)
+              VALUES ?
+            `;
+            //checking whether there is any new data which needs to be inserted to database
+            if (finaldata.length > 0) {
+              connection.query(insert, [finaldata], (err1, result) => {
+                if (err1) {
+                  console.log(`Error inserting data: ${err1}`);
+                  res.status(500).json({ error: 'Failed to insert earthquake data' });
+                } else {
+                  res.json({ message: 'Data inserted successfully' });
+                }
+              });
+            } else {
+              res.json({ message: 'There is no new data to be added' });
+            }
           }
         });
       }
     });
-    res.json({ message: 'Table created or already exists', data_insertion: isDataInserted ? 'Data inserted successfully' : 'Error inserting data' });
-
-
   } catch (error) {
     console.log(`Error fetching earthquake data: ${error}`);
     res.status(500).json({ error: 'Failed to fetch earthquake data' });
   }
 });
 
+// function compareData(earthQuakeData={}, dbData={}) {
+//   return earthQuakeData.DateTime === dbData.DateTime;
+// }
+
 // api to show earthquakes data in json format
 app.get('/api/earthquakes', (req, res) => {
   const selectQuery = 'SELECT * FROM earthquakes';
   connection.query(selectQuery, (err, rows) => {
     if (err) {
-      res.json({error: `Error fetching earthquake data:${err}`});
+      res.json({ error: `Error fetching earthquake data:${err}` });
       res.status(500).json({ error: 'Failed to fetch earthquake data' });
     } else {
       const geojsonData = {
         type: 'FeatureCollection',
         features: rows.map(earthquake => {
+          // console.log("rowwwww",row);
+          console.log("earthquake", earthquake);
+
           const { Coordinates, Region, Magnitude } = earthquake;
           const [lng, lat] = Coordinates.split(',').map(coord => parseFloat(coord));
           return {
